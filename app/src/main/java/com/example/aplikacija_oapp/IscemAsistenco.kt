7986db4c2fcb35028735dc7cfe5dd07a60b8db1c
Romih-Marija <marija.romih2@gmail.com>
2024-11-24 19:32:20 +0100

    s

--- app/src/main/java/com/example/aplikacija_oapp/IscemAsistenco.kt
+++ app/src/main/java/com/example/aplikacija_oapp/IscemAsistenco.kt
@@ -1,2 +1,71 @@
 package com.example.aplikacija_oapp
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun IscemAsistenco() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)  //
+        ,
+
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .size(80.dp)
+                .padding(bottom = 10.dp, top = 25.dp)
+        )
+        Text(
+            modifier = Modifier.padding(bottom = 20.dp),
+            text = "IŠČEM ASISTENCO",
+            fontSize = 21.sp,
+            fontWeight = FontWeight(500)
+        )
+
+        Column(
+            modifier = Modifier
+
+                .background(Color(0xFFBBDEFB))
+                .padding(top = 20.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text(
+                text = "OApp - Osebna asistenca - 2024",
+                modifier = Modifier.padding(bottom = 20.dp, top = 35.dp)
+            )
+        }
+    }
+}
